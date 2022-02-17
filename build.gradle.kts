10c09a85c8b357edd6472e1f83e10de658ec9588
y-tosa <y-tosa@pscsrv.co.jp>
2022-02-17 15:10:20 +0900

    2022-02-17

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,17 +16,31 @@ repositories {
 }
 
 dependencies {
-    val slackSdkVersion = "1.16.0"
+    // Slack
+    val slackSdkVersion = "1.18.0"
     implementation("com.slack.api:bolt:$slackSdkVersion")
     implementation("com.slack.api:bolt-jetty:$slackSdkVersion")
     implementation("com.slack.api:slack-api-model-kotlin-extension:$slackSdkVersion")
     implementation("com.slack.api:slack-api-client-kotlin-extension:$slackSdkVersion")
 
-    implementation("io.github.cdimascio:dotenv-kotlin:6.2.2")
-
+    // Logging
     implementation("ch.qos.logback:logback-classic:1.3.0-alpha12")
     implementation("io.github.microutils:kotlin-logging-jvm:2.1.21")
     implementation("net.logstash.logback:logstash-logback-encoder:7.0.1")
+
+    // Database
+    implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation("org.postgresql:postgresql:42.3.2")
+
+    // OR mapper
+    val exposedVersion = "0.37.3"
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
+
+    // Other
+    implementation("io.github.cdimascio:dotenv-kotlin:6.2.2")
 }
 
 tasks {
